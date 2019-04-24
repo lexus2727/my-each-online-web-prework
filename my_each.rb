ef2de18b31ece096447f59254405c2308b37ff87
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-24 23:08:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -8,11 +8,11 @@ def my_each(array)
       i = i + 1
     end #end yield
      
-  else
-      puts "No block was given!"
+  #else
+     # puts "No block was given!"
   end
   array
 end   
-  #my_each(list) {|i| puts #{i}}
+  #my_each(list) {|i|  #{i}}
 
   
